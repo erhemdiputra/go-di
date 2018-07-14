733eb4f80c3d1fabf8c64f1aabfb553d366b803b
Erhem <erhem.diputra@tokopedia.com>
2018-07-14 14:39:12 +0700

    read config from file

--- app.go
+++ app.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/erhemdiputra/go-di/config"
 	"github.com/erhemdiputra/go-di/handler"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	if err := config.Init(); err != nil {
+		log.Fatalf("[Err] Load Config File: %+v", err)
+	}
+
 	router := httprouter.New()
 
 	userHandler := handler.NewUserHandler(router)
 	userHandler.Serve()
 
-	log.Println("Listening on Port 8080")
-	http.ListenAndServe(":8080", router)
+	port := config.Main.Server.Port
+	log.Printf("Listening on Port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
 }
