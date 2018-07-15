60eecf9478f648995bfa0384b5a5d8c292be316f
Erhem <erhem.diputra@tokopedia.com>
2018-07-15 19:55:52 +0700

    remove newline

--- handler/user.handler.go
+++ handler/user.handler.go
@@ -7,7 +7,6 @@ import (
 	"github.com/erhemdiputra/go-di/controller"
 	"github.com/erhemdiputra/go-di/repository"
 	"github.com/erhemdiputra/go-di/service"
-
 	"github.com/julienschmidt/httprouter"
 )
 
