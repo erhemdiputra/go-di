c445b972660a6562bb97d487ab1eed6e2b317fad
Erhem <erhem.diputra@tokopedia.com>
2018-11-15 14:02:55 +0700

    update sql file

--- exercise-1.sql
+++ exercise-1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2018 at 01:54 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: Nov 15, 2018 at 07:00 AM
+-- Server version: 5.6.40
+-- PHP Version: 7.1.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `players` (
-  `id` int(11) NOT NULL,
+  `id` bigint(11) NOT NULL,
   `full_name` varchar(255) NOT NULL,
   `club` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -39,8 +39,29 @@ CREATE TABLE `players` (
 --
 
 INSERT INTO `players` (`id`, `full_name`, `club`) VALUES
-(1, 'Kylian Mbappe', 'PSG'),
-(2, 'Ousmane Dembelle', 'Barcelona');
+(1, 'Marcus Rashford', 'Manchester City'),
+(2, 'Ousmane Dembelle', 'Barcelona'),
+(3, '&lt;script&gt;&lt;alert&gt;asd&lt;/alert&gt;&lt;/script&gt;', '&lt;html&gt;&lt;head&gt;&lt;/head&gt;&lt;/html&gt;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullname`, `username`, `password`) VALUES
+(1, 'bob', 'bobby', 'bobby');
 
 --
 -- Indexes for dumped tables
@@ -53,6 +74,12 @@ ALTER TABLE `players`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,7 +87,13 @@ ALTER TABLE `players`
 -- AUTO_INCREMENT for table `players`
 --
 ALTER TABLE `players`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
